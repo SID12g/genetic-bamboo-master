818e2f8cc4d9a976fbaecb9dfa7b48a9af52c1d5
SID12g <sid12g@naver.com>
2024-10-08 22:33:11 +0900

    fix: webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "none",
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "./dist"),
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
     port: 3000,
   },
   plugins: [
